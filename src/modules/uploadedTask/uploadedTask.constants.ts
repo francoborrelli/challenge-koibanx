be33bca4ac90ff8d8672a9808611c4427567329f
Franco Borrelli <francoborrelli96@outlook.com>
2024-12-30 12:04:35 -0300

    add comment for formatters

--- src/modules/uploadedTask/uploadedTask.constants.ts
+++ src/modules/uploadedTask/uploadedTask.constants.ts
@@ -1,5 +1,8 @@
 import type { MappingTypes } from './uploadedTask.interfaces';
 
+// Constante que define los formateadores disponibles
+// La clave de primer nivel es un número que representa un tipo de formateador
+// El valor es un objeto que mapea nombres de campos a sus tipos correspondientes
 export const AVAILABLE_FORTATTERS: Record<number, Record<string, MappingTypes>> = {
   0: { name: 'String', age: 'Number', nums: 'Array<Number>' },
   1: { name: 'Number', age: 'String', 'nums?': 'Array<Number>' },
